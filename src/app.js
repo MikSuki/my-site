6acb4d4fa19a726712f18550d9a07e1b36a5452f
CHENG HAN <y3226999@gmail.com>
2020-09-02 16:39:47 +0800

    rm smth

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 import './index.css';
 import { Navbar } from './navbar';
 import { ContextContainer } from './context';

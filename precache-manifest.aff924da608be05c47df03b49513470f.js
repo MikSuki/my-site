db7293f212928ac747a48f153559a1b2725fb46e
MikSuki <34766025+MikSuki@users.noreply.github.com>
2023-09-30 00:39:55 +0800

    Updates

---
+++ precache-manifest.aff924da608be05c47df03b49513470f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c4d9544692dbef1ec394db675919a3",
+    "url": "/my-site/index.html"
+  },
+  {
+    "revision": "fb39e194bb98e9029500",
+    "url": "/my-site/static/css/main.aa9b7431.chunk.css"
+  },
+  {
+    "revision": "ff14891c642f590069bc",
+    "url": "/my-site/static/js/2.d9f67235.chunk.js"
+  },
+  {
+    "revision": "0cf5ba2c00bbc0b01089c885c528fd0c",
+    "url": "/my-site/static/js/2.d9f67235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb39e194bb98e9029500",
+    "url": "/my-site/static/js/main.a796d12f.chunk.js"
+  },
+  {
+    "revision": "aac20ac8aed1247e3513",
+    "url": "/my-site/static/js/runtime-main.40bc7211.js"
+  }
+]);
